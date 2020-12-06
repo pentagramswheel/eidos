44df1816dc0bae350e65cc0f9a979c6e939ed726
Wil Aquino <waquino@berkeley.edu>
2020-12-06 12:10:10 -0800

    Cleaned up PingUser, will finish it after Persistence

--- src/main/java/bot/PingUser.java
+++ src/main/java/bot/PingUser.java
@@ -28,6 +28,28 @@ public class PingUser extends ListenerAdapter {
     }
 
     /**
+     * Checks if an ID is given by one of the Sea+ members.
+     * @param userID the user's id.
+     * @return True if the ID is from Sea+.
+     *         False otherwise.
+     *
+     * Note: Clean up later.
+     */
+    private boolean isValidID(String userID) {
+        if (userID.equals("<@!297413857645953025>")
+            || userID.equals("<@!130196093039411200>")
+            || userID.equals("<@!440059670170959874>")
+            || userID.equals("<@!260923200697794562>")
+            || userID.equals("<@!208788589188743168>")
+            || userID.equals("<@!447491971188260885>")
+            || userID.equals("<@!724851006634197012>")
+            || userID.equals("<@!392856911562670085>")) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
      * Tests the ping functionality by taking in a ping.
      * @param e the ping to analyze. (Will only work in this case
      *          if the ping is given by "/ping").
@@ -36,39 +58,34 @@ public class PingUser extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent e) {
         String input = e.getMessage().getContentRaw();
         MessageChannel channel = e.getChannel();
-        String[] args = null;
+
+        String[] args;
+        String id;
+        int pings;
 
         try {
             args = input.split(" ", 3);
+
+            if (args[0].equals("--ping")) {
+                id = args[1];
+                pings = Integer.parseInt(args[2]);
+
+                if (pings > 20) {
+                    channel.sendMessage("If you ping them that much, had it "
+                            + "not been for the laws of these lands, they would "
+                            + "have slain you.").queue();
+                } else if (isValidID(id)) {
+                    ping(id, pings, channel);
+                } else {
+                    channel.sendMessage("You cannot ping that user!").queue();
+                }
+            }
         } catch (PatternSyntaxException pse) {
             channel.sendMessage("Invalid argument input.").queue();
-            pse.printStackTrace();
-            System.exit(0);
-        }
-
-        if (args[0].equals("--ping")) {
-            if (args.length != 3) {
-                channel.sendMessage("Please specify a user to ping and how "
-                        + "many times to ping them.").queue();
-                System.exit(0);
-            }
-            int pings = Integer.parseInt(args[2]);
-
-            ping(args[1], pings, channel);
-
-//            switch (args[1]) {
-//                case "Aloe":
-//                    ping("@aloeverai#0778", pings, channel);
-//                    break;
-//                case "Bren":
-//                    ping("@blondefox25#4828", pings, channel);
-//                    break;
-//                case "PTW":
-//                    ping("@PentagramsWheel#0168", pings, channel);
-//                    break;
-//                default:
-//                    channel.sendMessage("You cannot ping that user!").queue();
-//            }
+        } catch (NumberFormatException
+                | ArrayIndexOutOfBoundsException exc) {
+            channel.sendMessage("Please specify a user to ping and how "
+                    + "many times to ping them.").queue();
         }
     }
 }
