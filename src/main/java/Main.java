dd79c958cae99e3e55321f3c763aaf7d449fd6f2
Wil Aquino <waquino@berkeley.edu>
2020-12-05 23:39:56 -0800

    Testing pings through PingPong

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+/**
+ * @author  Wil Aquino
+ * Date:    December 5, 2020
+ * Project: Sea+ Bot
+ * Module:  Main.java
+ * Purpose: The entry point of the bot's backend.
+ */
 public class Main {
     public static void main(String[] args) {
-
+        JDABuilder jdaBuilder = JDABuilder.createDefault(
+                "Nzg1MDI1NDQyNDczNDQzMzM4.X8x16g.9qK7QopSxCU-2m4GP01UGC3GUPE");
+        JDA jda;
+        try {
+            jda = jdaBuilder.build();
+        } catch (LoginException le) {
+            le.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/PingPong.java
@@ -0,0 +1,24 @@
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+/**
+ * @author  Wil Aquino
+ * Date:    December 5, 2020
+ * Project: Sea+ Bot
+ * Module:  PingPong.java
+ * Purpose: A class for testing ping functions of the bot.
+ */
+public class PingPong extends ListenerAdapter {
+
+    /**
+     * Tests the ping functionality by taking in a ping.
+     * @param e the ping to analyze. (Will only work in this case
+     *          if the ping is given by "/ping").
+     */
+    @Override
+    public void onMessageReceived(MessageReceivedEvent e) {
+        if (e.getMessage().getContentRaw().equals("/ping")) {
+            e.getChannel().sendMessage("pong!").queue();
+        }
+    }
+}
